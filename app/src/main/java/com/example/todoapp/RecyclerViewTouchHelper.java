fd802bdef03f0cea7a1d216d95d7d32614aeb1fc
rafidenandra <rafiden01@gmail.com>
2021-10-03 09:22:59 +0700

    Adding swipe functionality

--- app/src/main/java/com/example/todoapp/RecyclerViewTouchHelper.java
+++ app/src/main/java/com/example/todoapp/RecyclerViewTouchHelper.java
@@ -2,13 +2,17 @@ package com.example.todoapp;
 
 import android.content.DialogInterface;
 import android.graphics.Canvas;
+import android.graphics.Color;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todoapp.Adapter.ToDoAdapter;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback {
 
@@ -28,7 +32,7 @@ public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback {
     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
         final int position = viewHolder.getAdapterPosition();
 
-        if (direction == ItemTouchHelper.RIGHT) {
+        if (direction == ItemTouchHelper.LEFT) {
             AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
             builder.setTitle("Delete Task");
             builder.setMessage("Are you sure?");
@@ -55,6 +59,14 @@ public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback {
 
     @Override
     public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                .addSwipeRightBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.colorPrimary))
+                .addSwipeRightActionIcon(R.drawable.ic_baseline_edit)
+                .addSwipeLeftBackgroundColor(Color.RED)
+                .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete)
+                .create()
+                .decorate();
+
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
     }
 }
